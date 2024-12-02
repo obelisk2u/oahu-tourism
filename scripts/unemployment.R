023e3b016501024a4db59d9ccf166f97f5f9e59b
obelisk2u <jdstout@gmail.com>
2024-12-01 20:48:49 -0500

    added second graph to unemployment tab

--- scripts/unemployment.R
+++ scripts/unemployment.R
@@ -13,6 +13,7 @@ plot_unemployment <- function() {
     na.omit() |>
     mutate(Value = as.numeric(Value)) |>
     mutate(Date = seq(as.Date("1990-01-01"), as.Date("2023-12-01"), by = "month"))
+    
   
   monthly_oahu <- ggplot(data, aes(x = Date, y = Value)) +
     geom_line(color = "blue") +
@@ -20,9 +21,24 @@ plot_unemployment <- function() {
     ylim(0,20) +
     theme_minimal()
   
+  
   data <- read_excel("./data/unemployment_us.xlsx")
   data <- data |>
-    slice(-c(11))
+    mutate(across(everything(), as.numeric)) |>
+    slice(-c(11)) |>
+    pivot_longer(
+      cols = -Year,
+      names_to = "month",     # Create a new column 'month' from column names
+      values_to = "rate"      # Create a new column 'rate' for values
+    ) |>
+    mutate(Date = seq(as.Date("2014-01-01"), as.Date("2023-12-01"), by = "month"))
   
-  return(monthly_oahu)
+  monthly_US <- ggplot(data, aes(x = Date, y = rate)) +
+    geom_line(color = "blue") +
+    labs(title = "US Unemployment Rate (2014-2023)", x = "Year", y = "Unemployment Rate (%)") +
+    ylim(0,20) +
+    theme_minimal()
+  
+  graph_final <- monthly_oahu + monthly_US
+  return(graph_final)
 }
